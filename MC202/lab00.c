cd81e020099609dfa3557aff806acde0abcc9b3b
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2021-03-31 09:39:23 -0300

    lab00.c e lab00_erros.c

---
+++ MC202/lab00.c
@@ -0,0 +1,30 @@
+/**
+	Código retirado do material entitulado "MC-202 Curso de C - Parte 1", do professor Rafael C. S. Schouery.
+*/
+
+#include <stdio.h>
+
+void le_vetor (int lista[] , int n) {
+	int i;
+
+	for (i = 0; i < n ; i++)
+		scanf ("%d", &lista[i]);
+}
+
+void imprime_positivos (int lista[] , int n) {
+	int i;
+
+	printf ("\nOs número positivos são: \n");
+	for (i = 0; i < n; i++)
+		if (lista[i] > 0)
+			printf ("%d\n", lista[i]);
+}
+
+int main () {
+	int lista [10];
+
+	le_vetor(lista, 10);
+	imprime_positivos(lista, 10);
+	
+	return 0;
+}
