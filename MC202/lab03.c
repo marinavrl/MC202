0972ded1fd37cc9d345f0ef84bc7e572d1ca024d
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2021-05-03 10:08:16 -0300

    lab03 inicio e bubblesort

---
+++ MC202/lab03.c
@@ -0,0 +1,8 @@
+/* Estruturas (structs), tambem conhecidos como registros, e apontadores na linguagem C. */
+/* Fazer a leitura, ordenacao e escrita de uma lista de registros. */
+/* Cada registro contem a informacao de um aluno: o RA, a data ou o nome.*/
+/* O criterio de ordenacao sera dado como parte da entrada e podera ser o RA, a data ou o nome.*/
+
+# include <string.h> /*comparacao de strings*/
+
+/* Ordenar com o Bubble Sort */
